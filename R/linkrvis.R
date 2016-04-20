90887b1b287a61618234c37664c6f9f2b22bba14
PeterDee <diakumis@wehi.edu.au>
2016-04-21 09:13:50 +1000

    Added linkrvis welcome message

---
+++ R/linkrvis.R
@@ -0,0 +1,11 @@
+#' linkrvis: A package for visualising output from genetic linkage analysis tools
+#'
+#'
+#' @docType package
+#' @name linkrvis
+NULL
+
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("linkrvis has landed..!")
+}

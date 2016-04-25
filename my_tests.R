c1b158c185edd0c971cd851a5e2df7d94c157d12
PeterDee <diakumis@wehi.edu.au>
2016-04-25 14:09:29 +1000

    Renamed files

--- my_tests.R
+++ my_tests.R
@@ -1,3 +1,14 @@
-data_path <- "~/Desktop/c10orf2/femepl_c10orf_extended_merlin"
-par <- linkrvis::read_partbl(file.path(data_path, "merlin_20_c10orf_extended-parametric.tbl"))
-npar <- linkrvis::read_npartbl(file.path(data_path, "merlin_20_c10orf_extended-nonparametric.tbl"))
+data_path <- "~/Desktop/c10orf2/merlin_output/joint_merlin"
+
+par <- linkrvis::read_merlin_partbl(file.path(data_path, "merlin_10_c10orf_extended-parametric.tbl"))
+npar <- linkrvis::read_merlin_npartbl(file.path(data_path, "merlin_10_c10orf_extended-nonparametric.tbl"))
+dat <- linkrvis::read_merlin_dat(file.path(data_path, "merlin_10_c10orf_extended.dat"))
+map <- linkrvis::read_merlin_map(file.path(data_path, "merlin_10_c10orf_extended.map"))
+ped <- read.table(file.path(data_path, "merlin_10_c10orf_extended.ped"),
+                  header = FALSE, stringsAsFactors = FALSE)
+
+str(npar)
+str(par)
+str(dat)
+str(map)
+str(ped)

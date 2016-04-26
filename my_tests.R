760dd1bfd666a6ca17fcac4f560aedf0a5086e06
PeterDee <diakumis@wehi.edu.au>
2016-04-26 11:48:37 +1000

    Added read ped function

--- my_tests.R
+++ my_tests.R
@@ -4,11 +4,21 @@ par <- linkrvis::read_merlin_partbl(file.path(data_path, "merlin_10_c10orf_exten
 npar <- linkrvis::read_merlin_npartbl(file.path(data_path, "merlin_10_c10orf_extended-nonparametric.tbl"))
 dat <- linkrvis::read_merlin_dat(file.path(data_path, "merlin_10_c10orf_extended.dat"))
 map <- linkrvis::read_merlin_map(file.path(data_path, "merlin_10_c10orf_extended.map"))
-ped <- read.table(file.path(data_path, "merlin_10_c10orf_extended.ped"),
-                  header = FALSE, stringsAsFactors = FALSE)
+ped <- linkrvis::read_merlin_ped(file.path(data_path, "merlin_10_c10orf_extended.ped"))
 
 str(npar)
 str(par)
 str(dat)
 str(map)
 str(ped)
+
+npartbl <- function(npartbl, ...) {
+  chrom <- get_chrom(npartbl)
+  max_lod <- get_max_lod(npartbl)
+  tab <- list(lods = npartbl$LOD,
+              snpname = npartbl$MARKER,
+              max_lod = max_lod,
+              chrom = chrom)
+  class(tab) <- "npartbl"
+  tab
+}

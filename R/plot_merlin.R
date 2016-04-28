8e6e43c4dbeb6e9fe7300548ab39ae0c85593b51
PeterDee <diakumis@wehi.edu.au>
2016-04-29 00:53:41 +1000

    Now plotting partbl

---
+++ R/plot_merlin.R
@@ -0,0 +1,13 @@
+#' Plot MERLIN Parametric LOD Scores
+#'
+#' Plots the LOD, ALPHA and HLOD
+#'
+#' @param partbl
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot.partbl <- function(partbl) {
+
+}
--- my_tests.R
+++ my_tests.R
@@ -1,10 +1,9 @@
 ## First try joint
+# dat <- linkrvis::read_merlin_dat(file.path(data_path, "merlin_10_c10orf_extended.dat"))
+# map <- linkrvis::read_merlin_map(file.path(data_path, "merlin_10_c10orf_extended.map"))
+# ped <- linkrvis::read_merlin_ped(file.path(data_path, "merlin_10_c10orf_extended.ped"))
 # par <- linkrvis::read_merlin_partbl(file.path(data_path, "merlin_10_c10orf_extended-parametric.tbl"))
-dat <- linkrvis::read_merlin_dat(file.path(data_path, "merlin_10_c10orf_extended.dat"))
-map <- linkrvis::read_merlin_map(file.path(data_path, "merlin_10_c10orf_extended.map"))
-ped <- linkrvis::read_merlin_ped(file.path(data_path, "merlin_10_c10orf_extended.ped"))
 # npar <- linkrvis::read_merlin_npartbl(file.path(data_path, "merlin_10_c10orf_extended-nonparametric.tbl"))
-
 
 ## npartbl
 npar <- linkrvis::npartbl(file.path(data_path, "merlin_10_c10orf_extended-nonparametric.tbl"))
@@ -16,6 +15,27 @@ summary(npar)
 ## partbl
 rm(list = ls())
 data_path <- "~/Desktop/c10orf2/merlin_output/joint_merlin"
-par <- linkrvis::read_merlin_partbl(file.path(data_path, "merlin_10_c10orf_extended-parametric.tbl"))
-
 partbl <- linkrvis::partbl(file.path(data_path, "merlin_10_c10orf_extended-parametric.tbl"))
+
+### Plotting
+require(ggplot2)
+require(dplyr)
+require(tidyr)
+
+plot_partbl <- function(partbl, variables = c("lod", "alpha", "hlod"))
+partbl$partbl %>%
+  dplyr::select(pos, c("lod", "alpha", "hlod"))
+  dplyr::select(pos, lod, alpha, hlod)
+  tidyr::gather(variable, value, lod:hlod) %>%
+  dplyr::filter(value > 0) %>%
+  ggplot(aes(x = pos, y = value, colour = variable)) +
+  geom_line() +
+  facet_wrap( ~ variable, ncol = 1, scales = "free_y")
+
+
+ggplot(data = DF, aes(x = pos, y = value, colour = key)) +
+  geom_line() +
+
+object.size(DF)
+object.size(partbl$partbl)
+
